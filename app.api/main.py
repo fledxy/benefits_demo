2d9c2501487c9a2f400df381923b490cbba6908d
fledxy <trinhthienbao2003@gmail.com>
2025-05-19 21:19:11 +0700

    commit Elastic-APM

--- app.api/main.py
+++ app.api/main.py
@@ -1,6 +1,19 @@
 from flask import Flask, jsonify
 
+from elasticapm.contrib.flask import ElasticAPM
+
 app = Flask(__name__)
+
+app.config['ELASTIC_APM'] = {
+  'SERVICE_NAME': 'sso-svc-prod',
+
+  'SECRET_TOKEN': 'bOD4kLSAaIwETXMkGh',
+
+  'SERVER_URL': 'https://8302e8dab20845b0a79774cc122a4364.apm.us-east-2.aws.elastic-cloud.com:443',
+
+  'ENVIRONMENT': 'my-environment',
+}
+apm = ElasticAPM(app)
 
 @app.route('/')
 def home():
@@ -13,6 +26,8 @@ def hello():
 @app.route('/api/health')
 def health():
     return jsonify({"status": "healthy"})
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8086) 
